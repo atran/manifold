b5b504bbacb30c14bfea0fcc72ea249352910f6a
anthony tran <atran.dummy@gmail.com>
2020-08-11 20:50:11 -0400

    use s3 as shrine store

--- api/config/initializers/55_shrine.rb
+++ api/config/initializers/55_shrine.rb
@@ -1,10 +1,18 @@
+require "dotenv"
+
 require "shrine/storage/file_system"
 require "shrine/storage/memory"
 require "shrine/storage/tus"
+require "shrine/storage/s3"
 
 Shrine.storages = {
   cache: Shrine::Storage::FileSystem.new("public", prefix: "system/cache"),
-  store: Shrine::Storage::FileSystem.new("public", prefix: "system")
+  store: Shrine::Storage::S3.new(
+    bucket: ENV["AWS_S3_BUCKET"],
+    region: ENV["AWS_S3_REGION"],
+    access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+  )
 }
 
 Shrine.storages[:tus] =
